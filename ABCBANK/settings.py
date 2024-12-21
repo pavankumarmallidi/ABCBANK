9432c6965f1c51d4533b6142be0b230c50d2f339
pavankumarmallidi <m.pavankumar255@gmail.com>
2024-12-21 22:13:06 +0530

    `Added whitenoise to INSTALLED_APPS and updated requirements.txt to reflect whitenoise version 6.8.2`

--- ABCBANK/settings.py
+++ ABCBANK/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account'
+    'account',
+    'whitenoise'
 ]
 
 MIDDLEWARE = [
